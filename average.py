05e98a7801704b6ed39930585582a5087d3f55ab
chenyanniii <42744112+chenyanniii@users.noreply.github.com>
2020-03-29 10:07:32 -0500

    Update average.py

--- average.py
+++ average.py
@@ -1,12 +1,14 @@
 ##  average.py 
-# Calculate sum from 1 to 20
+# Calculate average from 1 to 20
 #Part 1
 total = 0
 for i in range (1,21): 
 	total = i + total
-print("The sum from 1 to 20 is " + str(total) + ".")
+print(total)
+print("The average from 1 to 20 is " + str(total/len(range(1,21))) + ".")
+
 	
 #Part 2
 x = list(range(1,21))
 y = sum(x)
-print("The sum from 1 to 20 is " + str(y) + ".")
+print("The average from 1 to 20 is " + str(y/len(range(1,21))) + ".")
